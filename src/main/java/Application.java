01af0bd13f1e9d0653903f70935b56f5fd55f222
liva <liva.ok@gmail.com>
2022-03-21 15:00:01 +0300

    change Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,24 @@
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) {
-        Currency euro   = new Currency("Евро");
-        Currency dollar = new Currency("Доллар");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        MysqlDB.insertCurrencyCourse(euro);
-        MysqlDB.insertCurrencyCourse(dollar);
+        System.out.println("START...................................");
+        Class.forName("com.mysql.jdbc.Driver");
+        // Currency euro   = new Currency("Евро");
+        // Currency dollar = new Currency("Доллар");
+
+        // MysqlDB.insertCurrencyCourse(euro);
+        // MysqlDB.insertCurrencyCourse(dollar);
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.96.3:3306/mysql",
+            "root", "root");
+        System.out.println(connection);
+
+        System.out.println("END.....................................");
     }
 }
