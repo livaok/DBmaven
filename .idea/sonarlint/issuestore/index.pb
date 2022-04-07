4703ea64bb4157e79abdd8915ca40a3d4bc80305
liva <liva.ok@gmail.com>
2022-04-07 15:10:16 +0300

    Running application

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,12 @@
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+=
+.dockerignore,0/5/05a721ebe3ba0754116825ed1359ff1785f4bbac
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+R
+"src/test/java/TestApplication.java,6/1/61ca876d8a8c112d890d22bc0403f85de4c45d25
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,18 @@
-import com.mysql.jdbc.Driver;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException {
 
         System.out.println("START...................................");
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Currency euro   = new Currency("Евро");
-        Currency dollar = new Currency("Доллар");
-
-        MysqlDB.insertCurrencyCourse(euro);
-        MysqlDB.insertCurrencyCourse(dollar);
+        System.out.printf("Курс валюты %s на %s составляет %s%n", euro.getCurrency(), euro.getDate(), euro.getCourse());
+        // Currency dollar = new Currency("Доллар");
+        //
+        // MysqlDB.insertCurrencyCourse(euro);
+        // MysqlDB.insertCurrencyCourse(dollar);
 
         System.out.println("END.....................................");
+        //у цб есть api, получить json
     }
 }
